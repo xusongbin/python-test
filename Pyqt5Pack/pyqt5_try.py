dccf3b19e6e3e631f4ddc57e2f312e1af6389b13
SongbinXu <songbinxu@risinghf.com>
2019-05-17 15:04:30 +0800

    [Pyqt5Pack] Add drag event. Signed-off-by: SongbinXu <songbinxu@risinghf.com>

--- Pyqt5Pack/pyqt5_try.py
+++ Pyqt5Pack/pyqt5_try.py
@@ -1,4 +1,5 @@
 
+import re
 import sys
 from ui_pack import Ui_Form
 from PyQt5.QtWidgets import QApplication, QWidget
@@ -11,6 +12,7 @@ class Form(QWidget):
         self.work_ui = Ui_Form()
         self.work_ui.setupUi(self)
         self.work_ui.retranslateUi(self)
+        self.setAcceptDrops(True)
         self.show()
 
         self.work_cnt = 0
@@ -18,6 +20,7 @@ class Form(QWidget):
 
     def work_ui_init(self):
         self.work_ui.lineEdit.setText('')
+        self.work_ui.lineEdit.setAcceptDrops(False)
         self.work_ui.labelKeyText.setText('')
         self.work_ui.labelKeyValue.setText('')
         self.work_ui.pushButton.clicked.connect(self.fun_btn_add)
@@ -29,6 +32,23 @@ class Form(QWidget):
     def keyPressEvent(self, e):
         self.work_ui.labelKeyText.setText(e.text())
         self.work_ui.labelKeyValue.setText('%d' % e.key())
+
+    def dragEnterEvent(self, e):
+        _text = e.mimeData().text()
+        if re.match(r'file:///.*\..+', _text):  # this is a file path
+            _list = _text.split('\n')
+            _result = []
+            for _path in _list:
+                _path = _path.strip()
+                if _path == '':
+                    continue
+                _result.append(_path[8:])
+            for i in _result:
+                print('获取文件路径：%s' % i)
+            self.work_ui.lineEdit.setText(_result[0])
+        else:
+            print('获取文本内容：%s' % _text)
+        e.accept()
 
 
 if __name__ == '__main__':
