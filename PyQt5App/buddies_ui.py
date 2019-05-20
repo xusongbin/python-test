69e287053060ae32b23817c24068c3802737e5c2
SongbinXu <songbinxu@risinghf.com>
2019-05-20 14:04:34 +0800

    [PyQt5App] Add how to use buddies Signed-off-by: SongbinXu <songbinxu@risinghf.com>

---
+++ PyQt5App/buddies_ui.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'buddies_ui.ui'
+#
+# Created by: PyQt5 UI code generator 5.12
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.formLayoutWidget = QtWidgets.QWidget(Form)
+        self.formLayoutWidget.setGeometry(QtCore.QRect(60, 50, 266, 186))
+        self.formLayoutWidget.setObjectName("formLayoutWidget")
+        self.formLayout = QtWidgets.QFormLayout(self.formLayoutWidget)
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.formLayout.setObjectName("formLayout")
+        self.label = QtWidgets.QLabel(self.formLayoutWidget)
+        self.label.setObjectName("label")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label)
+        self.lineEdit = QtWidgets.QLineEdit(self.formLayoutWidget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.lineEdit)
+        self.label_2 = QtWidgets.QLabel(self.formLayoutWidget)
+        self.label_2.setObjectName("label_2")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label_2)
+        self.textEdit = QtWidgets.QTextEdit(self.formLayoutWidget)
+        self.textEdit.setObjectName("textEdit")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.textEdit)
+        self.label.setBuddy(self.lineEdit)
+        self.label_2.setBuddy(self.textEdit)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label.setText(_translate("Form", "作者(&a)"))
+        self.label_2.setText(_translate("Form", "简介(&b)"))
+
+
---
+++ PyQt5App/duddies.py
@@ -0,0 +1,21 @@
+
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication
+
+from buddies_ui import *
+
+
+class Form(QWidget):
+    def __init__(self):
+        super(Form, self).__init__()
+
+        self.form_ui = Ui_Form()
+        self.form_ui.setupUi(self)
+        self.form_ui.retranslateUi(self)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    run = Form()
+    sys.exit(app.exec_())
