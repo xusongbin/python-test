06e02cd039361c4df4e68c1a7411dee51556e27c
SongbinXu <songbinxu@risinghf.com>
2019-05-22 17:28:00 +0800

    [PyQt5App] How to edit QPlainTextEdit Signed-off-by: SongbinXu <songbinxu@risinghf.com>

---
+++ PyQt5App/pyQPlainTextEdit_ui.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'pyQPlainTextEdit_ui.ui'
+#
+# Created by: PyQt5 UI code generator 5.12
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(Form)
+        self.plainTextEdit.setGeometry(QtCore.QRect(5, 10, 386, 286))
+        self.plainTextEdit.setObjectName("plainTextEdit")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+
+
