7acabc1363315df5108887a43a19aab9e0a160f2
SongbinXu <songbinxu@risinghf.com>
2019-05-23 11:43:50 +0800

    [Aip] Add ocr SDK document Signed-off-by: SongbinXu <songbinxu@risinghf.com>

--- Aip/Ocr/apiOcr_ex.py
+++ Aip/Ocr/apiOcr_ex.py
@@ -1,6 +1,8 @@
 
 from aip import AipOcr
 
+SDK = 'https://cloud.baidu.com/doc/OCR/OCR-Python-SDK.html#.E9.80.9A.E7.94.A8.E6.96.87.E5.AD.97.E8.AF.86.E5.88.AB'
+
 APP_ID = '16318689'
 API_KEY = '0VPqwji3gCrEBrnPq2iQy68G'
 SECRET_KEY = 'GVecLAXZZfAavDW3crGPQFM0EeUppXAe'
