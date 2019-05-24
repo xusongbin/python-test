30c350e1543b606de57a585d8f06966a4bca0a59
SongbinXu <songbinxu@risinghf.com>
2019-05-24 18:27:01 +0800

    [PyQt5App] Add how to use QTableView Signed-off-by: SongbinXu <songbinxu@risinghf.com>

---
+++ PyQt5App/QTableView_ex/QTableView_ex.py
@@ -0,0 +1,37 @@
+
+# 表格视图实例
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+from QTableView_ex_ui import *
+
+
+class Example(QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.wui = Ui_Form()
+        self.wui.setupUi(self)
+        self.wui.retranslateUi(self)
+
+        model = QStandardItemModel(4, 4)
+        model.setHorizontalHeaderLabels(['标题1', '标题2', '标题3', '标题4'])
+
+        for row in range(4):
+            for column in range(4):
+                item = QStandardItem(str(row * 4 + column))
+                item.setTextAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+                model.setItem(row, column, item)
+        self.wui.tableView.setModel(model)
+        # self.wui.tableView.horizontalHeader().setStretchLastSection(True)
+        self.wui.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = Example()
+    w.show()
+    sys.exit(app.exec_())
---
+++ PyQt5App/QTableView_ex/QTableView_ex_ui.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'QTableView_ex_ui.ui'
+#
+# Created by: PyQt5 UI code generator 5.12
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.tableView = QtWidgets.QTableView(Form)
+        self.tableView.setGeometry(QtCore.QRect(60, 40, 256, 192))
+        self.tableView.setObjectName("tableView")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+
+
