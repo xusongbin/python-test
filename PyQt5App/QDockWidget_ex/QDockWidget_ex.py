31363da4733f37008a0e9b644cc1ec7979cc40cd
SongbinXu <songbinxu@risinghf.com>
2019-05-29 11:30:51 +0800

    [PyQt5App] Add how to use QDockWidget Signed-off-by: SongbinXu <songbinxu@risinghf.com>

---
+++ PyQt5App/QDockWidget_ex/QDockWidget_ex.py
@@ -0,0 +1,25 @@
+
+# Dock停靠控件实例1
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+from QDockWidget_ex_ui import *
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.wui = Ui_MainWindow()
+        self.wui.setupUi(self)
+        self.wui.retranslateUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = Example()
+    w.show()
+    sys.exit(app.exec_())
---
+++ PyQt5App/QDockWidget_ex/QDockWidget_ex_ui.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'QDockWidget_ex_ui.ui'
+#
+# Created by: PyQt5 UI code generator 5.12
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(500, 500)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit.setGeometry(QtCore.QRect(0, 0, 500, 455))
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 500, 23))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.dockWidget = QtWidgets.QDockWidget(MainWindow)
+        self.dockWidget.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea|QtCore.Qt.RightDockWidgetArea)
+        self.dockWidget.setObjectName("dockWidget")
+        self.dockWidgetContents = QtWidgets.QWidget()
+        self.dockWidgetContents.setObjectName("dockWidgetContents")
+        self.listWidget = QtWidgets.QListWidget(self.dockWidgetContents)
+        self.listWidget.setGeometry(QtCore.QRect(15, 35, 46, 231))
+        self.listWidget.setObjectName("listWidget")
+        item = QtWidgets.QListWidgetItem()
+        self.listWidget.addItem(item)
+        item = QtWidgets.QListWidgetItem()
+        self.listWidget.addItem(item)
+        item = QtWidgets.QListWidgetItem()
+        self.listWidget.addItem(item)
+        item = QtWidgets.QListWidgetItem()
+        self.listWidget.addItem(item)
+        self.dockWidget.setWidget(self.dockWidgetContents)
+        MainWindow.addDockWidget(QtCore.Qt.DockWidgetArea(2), self.dockWidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        __sortingEnabled = self.listWidget.isSortingEnabled()
+        self.listWidget.setSortingEnabled(False)
+        item = self.listWidget.item(0)
+        item.setText(_translate("MainWindow", "1"))
+        item = self.listWidget.item(1)
+        item.setText(_translate("MainWindow", "2"))
+        item = self.listWidget.item(2)
+        item.setText(_translate("MainWindow", "3"))
+        item = self.listWidget.item(3)
+        item.setText(_translate("MainWindow", "4"))
+        self.listWidget.setSortingEnabled(__sortingEnabled)
+
+
