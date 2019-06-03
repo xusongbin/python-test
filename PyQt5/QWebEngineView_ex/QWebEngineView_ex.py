567b0c6b39fea198944758e6584262198e2f60af
SongbinXu <songbinxu@risinghf.com>
2019-06-03 15:27:17 +0800

    [PyQt5] QWebEngineView_ex, Add some example. Signed-off-by: SongbinXu <songbinxu@risinghf.com>

--- PyQt5/QWebEngineView_ex/QWebEngineView_ex.py
+++ PyQt5/QWebEngineView_ex/QWebEngineView_ex.py
@@ -2,18 +2,24 @@
 # 使用内嵌网页引擎打开网址
 
 import sys
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtCore import *
 
 
-class Form(QWebEngineView):
+class Form(QWidget):
     def __init__(self):
         super(Form, self).__init__()
 
-        self.load(QUrl("http://stockpage.10jqka.com.cn/300059"))
+        self.resize(900, 500)
+
+        self.view = QWebEngineView(self)
+        self.view.resize(self.size())
+        url = 'http://stockpage.10jqka.com.cn/300059'
+        url = 'http://www.baidu.com'
+        url = QUrl(QFileInfo('web/index.html').absoluteFilePath())
+        self.view.load(url)
         self.show()
-        print(self.page().profile().cookieStore().loadAllCookies())
 
 
 if __name__ == '__main__':
