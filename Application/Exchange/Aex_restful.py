6895f2647d128239071bac5d4d37f4cbfc6df1c0
zaxusongbin <zaxusongbin@163.com>
2019-07-27 10:46:21 +0800

    [Application/Exchange/Aex_restful] Add account and order operation Signed-off-by: zaxusongbin <zaxusongbin@163.com>

--- Application/Exchange/Aex_restful.py
+++ Application/Exchange/Aex_restful.py
@@ -49,8 +49,8 @@ class Aex(object):
             self.log.error('{}\n{}'.format(e, traceback.format_exc()))
         return ''
 
+    # 返回自己當前的賬戶余額，包含系統支持的所有幣種。
     def do_get_mybalance(self):
-        # 返回自己當前的賬戶余額，包含系統支持的所有幣種。
         url = 'https://api.aex.zone/getMyBalance.php'
         ts = int(time())
         en_str = self.do_md5(ts)
@@ -69,8 +69,8 @@ class Aex(object):
             self.log.error('{}\n{}'.format(e, traceback.format_exc()))
         return {}
 
+    # 返回最高、最低交易行情和交易量，每15秒鐘更新
     def do_get_ticker(self, coin='bhd', kind='cnc'):
-        # 返回最高、最低交易行情和交易量，每15秒鐘更新
         url = 'https://api.aex.zone/ticker.php?c={}&mk_type={}'.format(coin, kind)
         try:
             req = request.Request(url, headers=self.headers)
@@ -81,8 +81,8 @@ class Aex(object):
             self.log.error('{}\n{}'.format(e, traceback.format_exc()))
         return {}
 
+    # 返回當前市場深度（委托掛單），其中 asks 是委賣單, bids 是委買單。返回30條。
     def do_get_depth(self, coin='bhd', kind='cnc'):
-        # 返回當前市場深度（委托掛單），其中 asks 是委賣單, bids 是委買單。返回30條。
         url = 'https://api.aex.zone/depth.php?c={}&mk_type={}'.format(coin, kind)
         try:
             req = request.Request(url, headers=self.headers)
@@ -93,13 +93,140 @@ class Aex(object):
             self.log.error('{}\n{}'.format(e, traceback.format_exc()))
         return {}
 
+    # 返回系統支持的歷史成交記錄，返回最新30條。
     def do_get_trades(self, coin='bhd', kind='cnc', more=False):
-        # 返回系統支持的歷史成交記錄，返回最新30條。
         url = 'https://api.aex.zone/trades.php?c={}&mk_type={}{}'.format(coin, kind, ('&tid=100' if more else ''))
         try:
             req = request.Request(url, headers=self.headers)
             resp = request.urlopen(req).read().decode('utf-8')
             data = json.loads(resp)
+            return data
+        except Exception as e:
+            self.log.error('{}\n{}'.format(e, traceback.format_exc()))
+        return {}
+
+    # 進行系統支持的所有數字貨幣的買賣操作。
+    def do_submit_order(self, price, amount, _type=1, coinname='bhd', mk_type='cnc'):
+        # 提交參數名	    描述
+        # type          1為買入掛單，2為賣出掛單，不可為空
+        # mk_type       交易區類型，必須為"btc|usdt|bitcny|cnc"其中之一，如"cnc", 不能為空
+        # price         價格，btc定價最多8位小數，不同幣種有所不同，以網站顯示為準
+        # amount        數量，最多6位小數
+        # coinname      幣名，比如BTC、DOGE、BTS
+        # 返回參數名	    描述
+        # succ	        掛單成功
+        # succ|123	    掛單成功，123為您掛單的ID
+        # overBalance	賬戶余額不足
+        #               其它返回表示不同的錯誤，情況太多，暫不羅列
+        url = 'https://api.aex.zone/submitOrder.php'
+        ts = int(time())
+        en_str = self.do_md5(ts)
+        data = {
+            'key': self.access_key,
+            'time': ts,
+            'md5': en_str,
+            'type': _type,
+            'mk_type': mk_type,
+            'price': price,
+            'amount': amount,
+            'coinname': coinname
+        }
+        try:
+            data = parse.urlencode(data).encode('utf-8')
+            req = request.Request(url, headers=self.headers, data=data)
+            resp = request.urlopen(req).read().decode('utf-8')
+            data = resp
+            return data
+        except Exception as e:
+            self.log.error('{}\n{}'.format(e, traceback.format_exc()))
+        return {}
+
+    # 進行自己委單的撤銷操作。
+    def do_cancel_order(self, order_id, coinname='bhd', mk_type='cnc'):
+        # 提交參數名	    描述
+        # order_id      要撤的單的ID
+        # mk_type       交易區類型，必須為"btc|usdt|bitcny|cnc"其中之一，如"cnc", 不能為空
+        # coinname      幣名，比如BTC、DOGE、BTS
+        # 返回參數名	    描述
+        # succ	        撤單成功
+        # overtime      該單不存在，或者已成交了
+        url = 'https://api.aex.zone/cancelOrder.php'
+        ts = int(time())
+        en_str = self.do_md5(ts)
+        data = {
+            'key': self.access_key,
+            'time': ts,
+            'md5': en_str,
+            'order_id': order_id,
+            'mk_type': mk_type,
+            'coinname': coinname
+        }
+        try:
+            data = parse.urlencode(data).encode('utf-8')
+            req = request.Request(url, headers=self.headers, data=data)
+            resp = request.urlopen(req).read().decode('utf-8')
+            data = resp
+            return data
+        except Exception as e:
+            self.log.error('{}\n{}'.format(e, traceback.format_exc()))
+        return {}
+
+    # 返回自己當前沒有成交的委單,已成交的掛單會顯示在成交記錄中。
+    def do_get_orderlist(self, coinname='bhd', mk_type='cnc'):
+        # 提交參數名	    描述
+        # mk_type       交易區類型，必須為"btc|usdt|bitcny|cnc"其中之一，如"cnc", 不能為空
+        # coinname      幣名，比如BTC、DOGE、BTS
+        # 返回參數名	    描述
+        # 掛單為空		[]
+        # 掛單不為空
+        # [{"id":"123", "type":"1", "coinname":"BTC", "order_amount":"23.232323", "order_price":"0.2929"},
+        # {"id":"123", "type":"1", "coinname":"LTC","order_amount":"23.232323", "order_price":"0.2929"}]
+        url = 'https://api.aex.zone/getOrderList.php'
+        ts = int(time())
+        en_str = self.do_md5(ts)
+        data = {
+            'key': self.access_key,
+            'time': ts,
+            'md5': en_str,
+            'mk_type': mk_type,
+            'coinname': coinname
+        }
+        try:
+            data = parse.urlencode(data).encode('utf-8')
+            req = request.Request(url, headers=self.headers, data=data)
+            resp = request.urlopen(req).read().decode('utf-8')
+            data = resp
+            return data
+        except Exception as e:
+            self.log.error('{}\n{}'.format(e, traceback.format_exc()))
+        return {}
+
+    # 返回自己的歷史成交記錄，每頁顯示30條。
+    def do_get_tradelist(self, page=1, coinname='bhd', mk_type='cnc'):
+        # 提交參數名	    描述
+        # mk_type       交易區類型，必須為"btc|usdt|bitcny|cnc"其中之一，如"cnc", 不能為空
+        # coinname      幣名，比如BTC、DOGE、BTS
+        # page          翻頁，0為第1頁，每頁30條，不填則默認為第1頁
+        # 返回參數名	    描述
+        # 為空		    []
+        # 不為空
+        # {id:123,buyer_id:123,seller_id:123,volume:123,price:123,coinname:"btc",time:"1990-02-22 12:12:12"}
+        url = 'https://api.aex.zone/getMyTradeList.php'
+        ts = int(time())
+        en_str = self.do_md5(ts)
+        data = {
+            'key': self.access_key,
+            'time': ts,
+            'md5': en_str,
+            'mk_type': mk_type,
+            'coinname': coinname,
+            'page': page
+        }
+        try:
+            data = parse.urlencode(data).encode('utf-8')
+            req = request.Request(url, headers=self.headers, data=data)
+            resp = request.urlopen(req).read().decode('utf-8')
+            data = resp
             return data
         except Exception as e:
             self.log.error('{}\n{}'.format(e, traceback.format_exc()))
@@ -111,4 +238,9 @@ if __name__ == '__main__':
     # print(aex.do_get_mybalance())
     # print(aex.do_get_ticker())
     # print(aex.do_get_depth())
-    print(aex.do_get_trades())
+    # print(aex.do_get_trades())
+
+    print(aex.do_submit_order(100, 1))
+    # print(aex.do_cancel_order(100, 1))
+    # print(aex.do_get_tradelist())
+
