e6fe121426aed68e5c15390ce675ad5a85c46a32
SongbinXu <songbinxu@risinghf.com>
2019-08-05 17:04:29 +0800

    [Application/DownloadImage] Add some debug Signed-off-by: SongbinXu <songbinxu@risinghf.com>

--- Application/DownloadImage/image.py
+++ Application/DownloadImage/image.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import re
+from time import sleep
 from lxml import etree
 from urllib import request
 
@@ -24,21 +25,20 @@ class AutoImage(object):
         if not os.path.isdir(self.record_path):
             os.mkdir(self.record_path)
         # 从网页获取所有主题的内容
-        # all_index = []
         # for theme in self.theme_list:
-        #     all_index += self.get_theme_index(theme, True)
+        #     self.get_theme_index(theme, True)
         # 从文件获取所有主题的内容
         all_index = self.read_theme_index()
-        print(all_index)
         # 从内容主页的列表获取图片链接
         for index in all_index:
             self.get_image_src(index, True)
         # 从文件读取图片链接并下载
-        self.read_src_to_download()
+        # self.read_src_to_download()
 
     def get_theme_index(self, theme, save=False):
         cur_url = self.web_url + theme
         record_name = self.record_path + '{}.csv'.format(theme.replace('/', ''))
+        write_log.debug('获取主题：{} 的所有专辑'.format(theme))
         index_list = []
         try:
             respond = request.urlopen(cur_url)
@@ -46,10 +46,14 @@ class AutoImage(object):
             page_tree = etree.HTML(page_source)
             page_last_html = page_tree.xpath('//div[@class="page-show"]/ul/li/a[text()="末页"]/@href')[0]
             page_last = int(re.findall(r'\d+', page_last_html)[0])
-            if save:
-                with open(record_name, 'a+') as f:
-                    f.write('{},{},{}\n'.format('地址', '数量', '名称'))
-            for idx in range(1, page_last + 1):
+        except Exception as e:
+            write_log.error('{}\n{}'.format(e, traceback.format_exc()))
+            return index_list
+        if save:
+            with open(record_name, 'a+') as f:
+                f.write('{},{},{}\n'.format('地址', '数量', '名称'))
+        for idx in range(1, page_last + 1):
+            try:
                 respond = request.urlopen(cur_url + '{}.html'.format(idx))
                 page_source = respond.read().decode('utf-8')
                 page_tree = etree.HTML(page_source)
@@ -57,13 +61,13 @@ class AutoImage(object):
                     addr = li.xpath('a/@href')[0]
                     num = re.findall(r'\d+', li.xpath('div/span/text()')[0])[0]
                     name = li.xpath('a/@title')[0]
-                    # print('地址：{} 数量：{} 名称：{}'.format(addr, num, name))
+                    write_log.debug('地址：{} 数量：{} 名称：{}'.format(addr, num, name))
                     if save:
                         with open(record_name, 'a+') as f:
                             f.write('{},{},{}\n'.format(addr, num, name))
                     index_list.append(addr)
-        except Exception as e:
-            write_log.error('{}\n{}'.format(e, traceback.format_exc()))
+            except Exception as e:
+                write_log.error('{}\n{}'.format(e, traceback.format_exc()))
         return index_list
 
     def read_theme_index(self):
@@ -83,6 +87,7 @@ class AutoImage(object):
                             continue
                         index = line.split(',')[0]
                         index_list.append(index)
+                        print('read_theme_index: {}'.format(index))
         except Exception as e:
             write_log.error('{}\n{}'.format(e, traceback.format_exc()))
         return index_list
@@ -97,17 +102,25 @@ class AutoImage(object):
                     f.write('IMAGE_SRC\n')
         while re.match(r'\d+.*\.html', page_suffix):
             page_url = self.web_url + page_theme + page_suffix
+            write_log.debug('get_image_src by:{}'.format(page_url))
+            page_source = ''
             try:
-                respond = request.urlopen(page_url)
+                respond = request.urlopen(page_url, timeout=3)
                 page_source = respond.read().decode('utf-8')
+                if 'bigpic' not in page_source:
+                    continue
                 page_tree = etree.HTML(page_source)
                 src = page_tree.xpath('//div[@id="bigpic"]/a/img/@src')[0]
+                print('get_image_src: {}'.format(src))
                 if save:
                     with open(self.src_path, 'a+') as f:
                         f.write('{}\n'.format(src))
                 page_suffix = page_tree.xpath('//div[@class="page"]/ul/li/a[text()="下一页"]/@href')[0]
             except Exception as e:
+                if page_source:
+                    write_log.debug(page_source)
                 write_log.error('{}\n{}'.format(e, traceback.format_exc()))
+                page_suffix = ''
         return src_list
 
     def download_image(self, dl_url, save_path):
@@ -131,8 +144,12 @@ class AutoImage(object):
                 if not re.match(r'/uploads/allimg/\d+/.*\..*', line):
                     continue
                 download_url = self.web_url + line
-                self.download_image(download_url, os.path.basename(download_url))
+                download_name = os.path.basename(download_url)
+                sta = self.download_image(download_url, download_name)
+                result = '成功' if sta else '失败'
+                print('download_name: {} {}'.format(download_name, result))
 
 
 if __name__ == '__main__':
     ai = AutoImage()
+    sleep(5)
