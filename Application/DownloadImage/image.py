c62c78ff86f7caa6aa542f650c5aa20bcbab5cee
zaxusongbin <zaxusongbin@163.com>
2019-08-06 00:19:22 +0800

    [Application/DownloadImage] Add proxy Signed-off-by: zaxusongbin <zaxusongbin@163.com>

--- Application/DownloadImage/image.py
+++ Application/DownloadImage/image.py
@@ -5,6 +5,7 @@ import re
 from time import sleep
 from lxml import etree
 from urllib import request
+from urllib import parse
 
 from md_logging import *
 
@@ -36,19 +37,45 @@ class AutoImage(object):
         if not os.path.isdir(self.record_path):
             os.mkdir(self.record_path)
         # 使用代理
-        # proxy_support = request.ProxyHandler({'http': '115.29.170.58:8118'})
+        # proxy_support = request.ProxyHandler({'http': '112.95.207.53:8888'})
         # opener = request.build_opener(proxy_support)
         # request.install_opener(opener)
         # 从网页获取所有主题的内容
-        for theme in self.theme_list:
-            self.get_theme_index(theme, True)
+        # for theme in self.theme_list:
+        #     self.get_theme_index(theme, True)
         # 从文件获取所有主题的内容
-        all_index = self.read_theme_index()
+        data_list = self.read_theme_index()
         # 从内容主页的列表获取图片链接
-        # for index in all_index:
-        #     self.get_image_src(index, True)
+        for index, num, name in data_list:
+            self.get_image_src(index, num, name, True)
         # 从文件读取图片链接并下载
         # self.read_src_to_download()
+
+    def get_source_by_url(self, surl, retry=4, method='GET'):
+        while retry > 0:
+            retry -= 1
+            try:
+                if method == 'GET':
+                    req = request.Request(surl, headers=self.headers)
+                    respond = request.urlopen(req, timeout=3)
+                    page_source = respond.read().decode('utf-8')
+                    return page_source
+                else:
+                    data = {'method': 0, 'host': surl, 'hideRAW': ''}
+                    data = parse.urlencode(data).encode('utf-8')
+                    req = request.Request('http://tool.chinaz.com/Tools/httptest.aspx', data=data, headers=self.headers)
+                    respond = request.urlopen(req, timeout=3)
+                    page_source = respond.read().decode('utf-8')
+                    page_tree = etree.HTML(page_source)
+                    page_context = page_tree.xpath('//div[@class="RtitCeCode"]/pre/text()')[0]
+                    return page_context
+            except Exception as e:
+                pass
+            if method == 'GET':
+                method = 'POST'
+            else:
+                method = 'GET'
+        return None
 
     def get_theme_index(self, theme, save=False):
         cur_url = self.web_url + theme
@@ -56,14 +83,14 @@ class AutoImage(object):
         write_log.debug('获取主题：{} 的所有专辑'.format(theme))
         index_list = []
         try:
-            req = request.Request(cur_url, headers=self.headers)
-            respond = request.urlopen(req)
-            page_source = respond.read().decode('utf-8')
+            page_source = self.get_source_by_url(cur_url)
+            if not page_source:
+                write_log.error('get_theme_index: get source failure')
+                return index_list
             page_tree = etree.HTML(page_source)
             page_last_html = page_tree.xpath('//div[@class="page-show"]/ul/li/a[text()="末页"]/@href')[0]
             page_last = int(re.findall(r'\d+', page_last_html)[0])
         except Exception as e:
-            write_log.error('cur_url:{}'.format(cur_url))
             write_log.error('{}\n{}'.format(e, traceback.format_exc()))
             return index_list
         if save:
@@ -71,9 +98,10 @@ class AutoImage(object):
                 f.write('{},{},{}\n'.format('地址', '数量', '名称'))
         for idx in range(1, page_last + 1):
             try:
-                req = request.Request(cur_url + '{}.html'.format(idx), headers=self.headers)
-                respond = request.urlopen(req)
-                page_source = respond.read().decode('utf-8')
+                page_source = self.get_source_by_url(cur_url + '{}.html'.format(idx))
+                if not page_source:
+                    write_log.error('get_theme_index: get source failure')
+                    return index_list
                 page_tree = etree.HTML(page_source)
                 for li in page_tree.xpath('//ul[@class="detail-list"]/li'):
                     addr = li.xpath('a/@href')[0]
@@ -89,7 +117,7 @@ class AutoImage(object):
         return index_list
 
     def read_theme_index(self):
-        index_list = []
+        data_list = []
         try:
             for theme in self.theme_list:
                 record_name = self.record_path + '{}.csv'.format(theme.replace('/', ''))
@@ -103,14 +131,14 @@ class AutoImage(object):
                             break
                         if not re.match(r'.*\.html.*', line):
                             continue
-                        index = line.split(',')[0]
-                        index_list.append(index)
-                        print('read_theme_index: {}'.format(index))
+                        line_list = line.split(',')
+                        data_list.append((line_list[0], line_list[1], line_list[2]))
+                        print('read_theme_index: {} {} {}'.format(line_list[0], line_list[1], line_list[2]))
         except Exception as e:
             write_log.error('{}\n{}'.format(e, traceback.format_exc()))
-        return index_list
+        return data_list
 
-    def get_image_src(self, image_url, save=False):
+    def get_image_src(self, image_url, num, name, save=False):
         src_list = []
         page_theme, page_suffix = os.path.split(image_url)
         page_theme += '/'
@@ -118,19 +146,20 @@ class AutoImage(object):
             if not os.path.isfile(self.src_path):
                 with open(self.src_path, 'w') as f:
                     f.write('IMAGE_SRC\n')
+        write_log.debug('get_image_src:{} {} {}'.format(image_url, num, name))
         while re.match(r'\d+.*\.html', page_suffix):
             page_url = self.web_url + page_theme + page_suffix
-            write_log.debug('get_image_src by:{}'.format(page_url))
             page_source = ''
             try:
-                req = request.Request(page_url, headers=self.headers)
-                respond = request.urlopen(req, timeout=3)
-                page_source = respond.read().decode('utf-8')
+                page_source = self.get_source_by_url(page_url)
+                if not page_source:
+                    write_log.error('get_image_src: get source failure')
+                    return src_list
                 if 'bigpic' not in page_source:
-                    continue
+                    return src_list
                 page_tree = etree.HTML(page_source)
                 src = page_tree.xpath('//div[@id="bigpic"]/a/img/@src')[0]
-                print('get_image_src: {}'.format(src))
+                write_log.debug('get_image_src:{} {}'.format(page_url, src))
                 if save:
                     with open(self.src_path, 'a+') as f:
                         f.write('{}\n'.format(src))
@@ -139,14 +168,17 @@ class AutoImage(object):
                 if page_source:
                     write_log.debug(page_source)
                 write_log.error('{}\n{}'.format(e, traceback.format_exc()))
-                page_suffix = ''
+                write_log.debug('get_image_src:{} {}'.format(page_url, 'ERROR'))
+                return src_list
         return src_list
 
-    def download_image(self, dl_url, save_path):
+    def download_image(self, dl_url, save_path, retry=5):
         save_path = self.local_path + save_path
         try:
-            request.urlretrieve(dl_url, save_path)
-            return True
+            while retry > 0:
+                retry -= 1
+                request.urlretrieve(dl_url, save_path)
+                return True
         except Exception as e:
             write_log.error('{}\n{}'.format(e, traceback.format_exc()))
         return False
@@ -166,7 +198,7 @@ class AutoImage(object):
                 download_name = os.path.basename(download_url)
                 sta = self.download_image(download_url, download_name)
                 result = '成功' if sta else '失败'
-                print('download_name: {} {}'.format(download_name, result))
+                write_log.debug('download_name: {} {}'.format(download_name, result))
 
 
 if __name__ == '__main__':
