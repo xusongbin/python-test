2dca57ceda63258797d04fd8f666d67ebcaddf39
zaxusongbin <zaxusongbin@163.com>
2019-11-03 22:02:44 +0800

    [tkinter/single_window] 1.py Add label/entry/button use gird Signed-off-by: zaxusongbin <zaxusongbin@163.com>

--- tkinter/single_window/1.py
+++ tkinter/single_window/1.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import tkinter as tk
+from tkinter import *
+import random
 
 
-class MainWindow(tk.Tk):
+class MainWindow(Tk):
     def __init__(self):
         super().__init__()
         self.title('test window')
         self.geometry('400x300')
         self.resizable(0, 0)
 
-        self.label_usr = tk.Label(self, text='用户名：', width=6)
-        self.label_usr.place(x=10, y=10)
+        self.label_usr = Label(self, text='1', width=10)
+        self.label_usr.grid(row=1, column=1)
 
-        self.entry_usr = tk.Entry(self, text='', width=18)
-        self.entry_usr.place(x=60, y=10)
+        self.entry_usr_ver = StringVar()
+        self.entry_usr = Entry(self, textvariable=self.entry_usr_ver, width=20)
+        self.entry_usr.grid(row=1, column=2)
+        self.entry_usr.focus()
 
-        self.button_usr = tk.Button(self, text='确认', width=6, height=1, command=self.on_btn_usr_clicked)
-        self.button_usr.place(x=200, y=6)
+        self.button_usr = Button(self, text='确认', width=6, height=1, command=self.on_btn_usr_clicked)
+        self.button_usr.grid(row=1, column=3)
 
     def on_btn_usr_clicked(self):
         print('button clicked')
+        msg = str(random.random()*10000)
+        print(msg)
+        self.entry_usr_ver.set(msg)
+        # self.entry_usr.set(msg)
 
     def run(self):
         self.mainloop()
