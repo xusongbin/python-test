408b9600a99cae919852de993f6d1b26db4f0bb5
songbinxu@risinghf.com <songbinxu@risinghf.com>
2020-02-24 18:12:48 +0800

    [Application/Chicangliang] Update Signed-off-by: songbinxu@risinghf.com <songbinxu@risinghf.com>

--- Application/Chicangliang/run.py
+++ Application/Chicangliang/run.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pandas as pd
 import requests
 import pyperclip
 from lxml import etree
 from traceback import format_exc
-from time import strftime, localtime, time
+from time import strftime, localtime, time, sleep
 
 
 headers = {
@@ -92,48 +91,16 @@ def put_clipboard(date='2020-02-21', top=10):
     return False
 
 
-def change_excel():
-    ex_name = '持仓量.xlsx'
-    ot_name = '持仓量1.xlsx'
-    ex_df = pd.read_excel(ex_name)
-    ex_list = ex_df.values.tolist()
-    print(ex_list)
-    resp_name = []
-    resp_data = []
-    for row in ex_list:
-        resp_name.append(row[1])
-    for name in resp_name:
-        data = 0
-        for row in ex_list:
-            if row[1] == name:
-                data += int(row[2])
-            if row[4] == name:
-                data += int(row[5])
-        resp_data.append(data)
-    resp = {}
-    for idx, name in enumerate(resp_name):
-        resp[name] = resp_data[idx]
-    # print(resp)
-    resp = sorted(resp.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
-    print(resp)
-    for idx, row in enumerate(ex_list):
-        ex_list[idx].append(idx+1)
-        ex_list[idx].append(resp[idx][0])
-        ex_list[idx].append(resp[idx][1])
-    print(ex_list)
-    dd = pd.DataFrame(ex_list)
-    print(dd)
-    dd.to_excel(ot_name, index=None)
-
-
 if __name__ == '__main__':
-    date = input('请输入日期（默认获取昨天数据）：')
+    yesterday = strftime("%Y-%m-%d", localtime(time() - 24*3600))
+    pyperclip.copy(yesterday)
+    date = input('请输入日期（默认昨天的日期，可黏贴）：')
     date = date.strip()
     if not date:
-        date = strftime("%Y-%m-%d", localtime(time() - 24*3600))
-    print(date)
+        date = yesterday
     result = put_clipboard(date)
     if result:
         print('已获取{}数据到剪切板'.format(date))
     else:
         print('获取数据异常，请检查网络及日期是否正确。（2020-02-21）')
+    sleep(3)
--- Application/Chicangliang/test_api.py
+++ Application/Chicangliang/test_api.py
@@ -4,9 +4,9 @@
 import baostock as bs
 
 
-if __name__ == '__main__':
+def get_day():
     lg = bs.login()
-    rs = bs.query_history_k_data_plus(
+    rs = bs.query_history_k_data(
         'sh.000001',
         "date,code,open,high,low,close,preclose,volume,amount",
         '2020-02-24',
@@ -15,3 +15,13 @@ if __name__ == '__main__':
     )
     print(rs.get_data())
     bs.logout()
+
+
+def get_real():
+    bs.login()
+    rs = bs.login_real_time(user_id='anonymous', password='123456')
+    bs.logout()
+
+
+if __name__ == '__main__':
+    get_real()
